c84b73882b9d7df9fa092838eb76eba08401e2eb
Susan Khadka <susankhadka708@gmail.com>
2023-05-10 23:28:11 +0530

    Completed level 1 milestone

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,54 @@
 
 import AppContainer from "./AppContainer";
 import Header from "./Header";
-import React from "react";
+import React, { FormEvent } from "react";
 
 function App() {
   const formFields = [
-    { id: 1, label: "First Name" },
-    { id: 2, label: "Last Name" },
-    { id: 3, label: "Email" },
+    { id: 1, label: "First Name", type: "text" },
+    { id: 2, label: "Last Name", type: "text" },
+    { id: 3, label: "Email", type: "email" },
+    { id: 4, label: "Date of Birth", type: "date" },
+    { id: 5, label: "Contact No", type: "tel" },
   ];
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Form Submitted");
+  };
 
   return (
     <AppContainer>
-      <div className=" p-4 mx-auto bg-white shadow-lg rounded-xl">
+      <div className="w-1/4 p-4 mx-auto bg-white shadow-lg rounded-xl">
         <Header title="Welcome to #react-with-typescript and tailwind" />
-        {formFields.map((fields) => {
-          return (
-            <React.Fragment key={fields.id}>
-              <label className="" htmlFor="">
-                {fields.label}
-              </label>
-              <input
-                className="border border-gray-200 rounded-lg p-2 m-2 w-full"
-                type="text"
-              />
-            </React.Fragment>
-          );
-        })}
+        <form
+          className="px-2 mt-4"
+          onSubmit={(event) => {
+            handleSubmit(event);
+          }}
+        >
+          {formFields.map((fields) => {
+            return (
+              <React.Fragment key={fields.id}>
+                <label className="" htmlFor="">
+                  {fields.label}
+                </label>
+                <input
+                  className="border border-gray-200 rounded-lg p-2 mt-2 mb-4 w-full"
+                  type={fields.type}
+                />
+              </React.Fragment>
+            );
+          })}
+          <div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white rounded-lg px-4 py-2 m-2 text-lg"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </AppContainer>
   );
