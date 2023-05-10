4df929cb7b091c1ff603ad12f89ff93f669883ef
Susan Khadka <susankhadka708@gmail.com>
2023-05-10 22:50:37 +0530

    Completed 1st level

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,42 @@ import React from "react";
 
 function App() {
   const formFields = [
-    { id: 1, label: "First Name" },
-    { id: 2, label: "Last Name" },
-    { id: 3, label: "Email" },
+    { id: 1, label: "First Name", type: "text" },
+    { id: 2, label: "Last Name", type: "text" },
+    { id: 3, label: "Email", type: "email" },
+    { id: 4, label: "Date of Birth", type: "date" },
+    { id: 5, label: "Phone", type: "tel" },
   ];
+
+  const handleSubmit = (event:any) =>{
+    event.preventDefault();
+    console.log("Form Submitted");
+  }
 
   return (
     <AppContainer>
-      <div className=" p-4 mx-auto bg-white shadow-lg rounded-xl">
+      <div className="w-1/3 p-4 mx-auto bg-white shadow-lg rounded-xl">
         <Header title="Welcome to #react-with-typescript and tailwind" />
-        {formFields.map((fields) => {
-          return (
-            <React.Fragment key={fields.id}>
-              <label className="" htmlFor="">
-                {fields.label}
-              </label>
-              <input
-                className="border border-gray-200 rounded-lg p-2 m-2 w-full"
-                type="text"
-              />
-            </React.Fragment>
-          );
-        })}
+        <form onSubmit={(event)=>{handleSubmit(event)}}>
+          {formFields.map((fields) => {
+            return (
+              <React.Fragment key={fields.id}>
+                <label className="" htmlFor="">
+                  {fields.label}
+                </label>
+                <input
+                  className="border border-gray-200 rounded-lg p-2 mx-auto mt-2 mb-4 w-full"
+                  type={fields.type}
+                />
+              </React.Fragment>
+            );
+          })}
+          <input
+            className="border m-2 px-4 hover:cursor-pointer py-2 rounded-md bg-blue-600 text-white font-semibold text-lg"
+            type="submit"
+            value="Submit"
+          />
+        </form>
       </div>
     </AppContainer>
   );
--- src/Header.tsx
+++ src/Header.tsx
@@ -8,7 +8,7 @@ function Header(props: {title: string}) {
   return (
     <div className="flex gap-2 justify-center items-center">
       <img src={logo} style={spinStyle} className=" w-16 h-16" alt="logo" />
-      <h1 className="text-center text-xl flex-1">
+      <h1 className="text-center text-xl font-medium flex-1">
         {props.title}
       </h1>
     </div>
