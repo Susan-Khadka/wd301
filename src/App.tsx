f0eca35fa42b4825861fa6aade0cc0ed521333e3
Susan Khadka <susankhadka708@gmail.com>
2023-05-11 00:56:18 +0530

    Introduction to react hooks in functional components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 
 import AppContainer from "../src/hoc/AppContainer";
 import Header from "./Header";
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
+import Home from "./components/Home";
 
 function App() {
   const formFields = [
@@ -18,38 +19,68 @@ function App() {
     console.log("Form Submitted");
   };
 
+  const [state, setState] = useState("HOME");
+
+  const openForm = () => {
+    setState("FORM");
+  };
+
+  const closeForm = () => {
+    setState("HOME");
+  }
+
   return (
     <AppContainer>
-      <div className="w-1/4 p-4 mx-auto bg-white shadow-lg rounded-xl">
+      <div className="w-1/3 p-4 mx-auto bg-white shadow-lg rounded-xl">
         <Header title="Welcome to #react-with-typescript and tailwind" />
-        <form
-          className="px-2 mt-4"
-          onSubmit={(event) => {
-            handleSubmit(event);
-          }}
-        >
-          {formFields.map((fields) => {
-            return (
-              <React.Fragment key={fields.id}>
-                <label className="" htmlFor="">
-                  {fields.label}
-                </label>
-                <input
-                  className="border border-gray-200 rounded-lg p-2 mt-2 mb-4 w-full"
-                  type={fields.type}
-                />
-              </React.Fragment>
-            );
-          })}
-          <div>
-            <button
-              type="submit"
-              className="bg-blue-500 text-white rounded-lg px-4 py-2 m-2 text-lg"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+        {state === "HOME" ? (
+          <>
+            <Home />
+            <div className="flex justify-center items-center my-5">
+              <button
+                className="border w-full py-2 px-4 rounded-lg bg-blue-500 text-white"
+                onClick={openForm}
+              >
+                Open Form
+              </button>
+            </div>
+          </>
+        ) : (
+          <form
+            className="px-2 mt-4"
+            onSubmit={(event) => {
+              handleSubmit(event);
+            }}
+          >
+            {formFields.map((fields) => {
+              return (
+                <React.Fragment key={fields.id}>
+                  <label className="" htmlFor="">
+                    {fields.label}
+                  </label>
+                  <input
+                    className="border border-gray-200 rounded-lg p-2 mt-2 mb-4 w-full"
+                    type={fields.type}
+                  />
+                </React.Fragment>
+              );
+            })}
+            <div className="flex gap-x-4">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white rounded-lg px-4 py-2 m-2 text-lg"
+              >
+                Submit
+              </button>
+              <button
+                className="bg-blue-500 text-white rounded-lg px-4 py-2 m-2 text-lg"
+                onClick={closeForm}
+              >
+                Close Form
+              </button>
+            </div>
+          </form>
+        )}
       </div>
     </AppContainer>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import logo from "../logo.svg";
 
 function Home() {
   return (
-    <div>
-      He;;;p
+    <div className="flex mt-5">
+      <img className="h-48" src={logo} alt="logo" />
+      <div className="h-48 flex justify-center items-center">
+        <p className="text-2xl font-medium"> Welcome to the home page</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
