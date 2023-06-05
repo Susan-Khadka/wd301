ef79aec658c948556f27530a8a20245dc0144acf
Susan Khadka <susankhadka708@gmail.com>
2023-06-05 19:57:01 +0545

    Implemented Multiselect dropdown

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -4,8 +4,6 @@ export type FormData = {
     formFields: FormField[];
 }
 
-
-
 export type TextField = {
     kind: "text";
     id: number;
@@ -34,21 +32,35 @@ export type checkboxField = {
     kind: "checkbox";
     id: number;
     label: string;
-    options: string[];
-    value: string;
+    options: Option[];
+    value: Option[];
 }
 
 export type TextArea = {
     kind: "textarea";
+    type: "textarea";
     id: number;
     label: string;
     value: string;
 }
 
+export type Option = {
+    id: string;
+    value: string;
+}
+
+export type MultiSelectDrop = {
+    kind: "multiselectdrop";
+    id: number;
+    label: string;
+    options: Option[];
+    value: Option[];
+}
+
 export type textFieldTypes = "text" | "email" | "password" | "number" | "tel" | "date";
 
-export type otherFieldTypes = "dropdown" | "radio" | "checkbox" | "textarea";
+export type otherFieldTypes = "dropdown" | "radio" | "checkbox" | "textarea" | "multiselectdrop";
 
 export type allFieldTypes = textFieldTypes | otherFieldTypes;
 
-export type FormField = TextField | DropdownField | radioField | checkboxField | TextArea ;
+export type FormField = TextField | DropdownField | radioField | checkboxField | TextArea | MultiSelectDrop;
