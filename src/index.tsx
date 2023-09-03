69d2637ffe53d7d00774f253febe6cf09ec7fec1
codexsusan <susankhadka708@gmail.com>
2023-09-03 12:53:38 +0530

    Working with sentry

--- src/index.tsx
+++ src/index.tsx
@@ -3,34 +3,37 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-// import * as Sentry from "@sentry/react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as Sentry from "@sentry/react";
 
-// if (process.env.REACT_APP_SENTRY_DSN) {
-//   Sentry.init({
-//     dsn: "https://ebf8b5ba20f1419789e92148699292a6@o4505375289442304.ingest.sentry.io/4505375290753024",
-//     integrations: [
-//       new Sentry.BrowserTracing({
-//         // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-//         tracePropagationTargets: [
-//           "localhost",
-//           /^https:\/\/yourserver\.io\/api/,
-//         ],
-//       }),
-//       new Sentry.Replay(),
-//     ],
-//     // Performance Monitoring
-//     tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-//     // Session Replay
-//     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-//     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-//   });
-// }
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: "https://ebf8b5ba20f1419789e92148699292a6@o4505375289442304.ingest.sentry.io/4505375290753024",
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: [
+          "localhost",
+          /^https:\/\/yourserver\.io\/api/,
+        ],
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(<App />);
+
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
